bee45fc4e314a75f5dd3e1b65d7752ee28cba8ca
Adrian McBroom <adrianmcb2000@gmail.com>
2024-02-21 16:29:44 -0800

    Filled questions.html

--- app.py
+++ app.py
@@ -7,3 +7,13 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
+
+
+@app.get('/')
+def index():
+    return render_template('questions.html', prompts=silly_story.prompts)
+
+
+@app.get('/results')
+def show_results():
+    return render_template('results.html')
