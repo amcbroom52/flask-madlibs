f39fc77ee16b7d16976d4e6d8ede99c378e7fdf5
David Sapiro <dsap48@gmail.com>
2024-02-21 17:12:05 -0800

    finsihed main exercise

--- app.py
+++ app.py
@@ -11,9 +11,17 @@ debug = DebugToolbarExtension(app)
 
 @app.get('/')
 def index():
+    """Loads root page with prompts from a Story instance."""
+
     return render_template('questions.html', prompts=silly_story.prompts)
 
 
 @app.get('/results')
 def show_results():
-    return render_template('results.html')
+    """Accepts user input as a query string for each prompt. Then creates and
+    displays mad lib story utilizing the template from the Story instance
+    and user's answers."""
+
+    result_text = silly_story.get_result_text(request.args)
+
+    return render_template('results.html', story=result_text)
