a3c899d338117eb5143f03461328ccb9ba4a0d0b
Adrian McBroom <adrianmcb2000@gmail.com>
2024-02-21 18:02:09 -0800

    finished step one of futher study

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
 
-from stories import silly_story
+from stories import story_list
+
+selected_story = {}
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
@@ -11,9 +13,22 @@ debug = DebugToolbarExtension(app)
 
 @app.get('/')
 def index():
+    return render_template('index.html', stories=story_list)
+
+
+@app.get('/prompt')
+def get_prompts():
     """Loads root page with prompts from a Story instance."""
 
-    return render_template('questions.html', prompts=silly_story.prompts)
+    story_name = request.args['stories']
+
+    global selected_story
+
+    for story in story_list:
+        if story_name == story.name:
+            selected_story = story
+
+    return render_template('questions.html', prompts=selected_story.prompts)
 
 
 @app.get('/results')
@@ -22,6 +37,6 @@ def show_results():
     displays mad lib story utilizing the template from the Story instance
     and user's answers."""
 
-    result_text = silly_story.get_result_text(request.args)
+    result_text = selected_story.get_result_text(request.args)
 
     return render_template('results.html', story=result_text)
